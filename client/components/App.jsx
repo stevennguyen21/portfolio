4f6d203fa0332ad4dd57df55434bfb8c2959ba02
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-26 19:16:42 -0700

    compile scss loader and tested to make sure working

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/main.scss';
+
+const App = () => {
+    return (
+        <div className="test">
+            Hello!
+        </div>
+    )
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-ReactDOM.render(<div>Hi from app</div>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
