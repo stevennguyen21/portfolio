c8eb446cd9ccf848abde938e2e51261bc7d4cc1a
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-28 22:48:30 -0700

    changed overlay variable to after pseudo element

--- client/components/Mainpage.jsx
+++ client/components/Mainpage.jsx
@@ -2,45 +2,40 @@ import React from 'react';
 
 const Mainpage = () => {
     return (
-        <div>
-            <div className="overlay-color">
+        <header>
+            <div className="menu-btn">
+                <div className="btn-line"></div>
+                <div className="btn-line"></div>
+                <div className="btn-line"></div>
             </div>
-            <header>
-                <div className="menu-btn">
-                    <div className="btn-line"></div>
-                    <div className="btn-line"></div>
-                    <div className="btn-line"></div>
+            <nav className="menu">
+                <div className="menu-branding">
+                    <div className="portrait"></div>
                 </div>
-                <nav className="menu">
-                    <div className="menu-branding">
-                        <div className="portrait"></div>
-                    </div>
-                    <ul className="menu-nav">
-                        <li className="nav-item">
-                            <a href="/" className="nav-link">
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/about" className="nav-link">
-                                About Me
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/projects" className="nav-link">
-                                Projects
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/contact" className="nav-link">
-                                Contact Me
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-
-        </div>
+                <ul className="menu-nav">
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/about" className="nav-link">
+                            About Me
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/projects" className="nav-link">
+                            Projects
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/contact" className="nav-link">
+                            Contact Me
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
