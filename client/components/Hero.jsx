15e49bcb7498428e5be9a32c2074573d34532516
Steven Nguyen <steeven.nguyen1102@gmail.com>
2021-03-30 21:41:50 -0700

    finished basic layout of portfolio

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     <p>Meet Steven Nguyen</p>
                 </div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="686" height="688" viewBox="0 0 686 688">
+            <svg className="hero-design" xmlns="http://www.w3.org/2000/svg" width="686" height="688" viewBox="0 0 686 688">
                 <g id="blockdesign" transform="translate(-935 -289)">
                     <rect id="Rectangle_2" data-name="Rectangle 2" width="172" height="172" rx="19" transform="translate(1277 289)" fill="#6e00ff"/>
                     <rect id="Rectangle_10" data-name="Rectangle 10" width="172" height="172" rx="86" transform="translate(1277 461)" fill="#ff64cb"/>
